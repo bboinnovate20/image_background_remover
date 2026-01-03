5c91d8f0ae29d0704450f049397c434adc5142f7
Ibrahim <bboinnovate20@gmail.com>
2026-01-03 08:47:40 +0100

    fix

--- lib/assets.dart
+++ lib/assets.dart
@@ -1,6 +1,7 @@
 class Assets {
   static const modelPath =
       // 'packages/image_background_remover/assets/model.onnx';
-      'assets/model/model.onnx'
+      'assets/model/model.onnx';
 }
+
 // /Users/bboitech/projects/digitallab/assets/model.onnx
